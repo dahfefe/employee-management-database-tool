69c11814a712ba95e14757207fccc3f8a6581156
Felix Teng <fsmunozteng@gmail.com>
2024-04-25 06:25:03 -0700

    finalized and added seed code for sql testing our tables

--- db/schema.sql
+++ db/schema.sql
@@ -4,15 +4,15 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (   
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-  department_name VARCHAR(30) NOT NULL   
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    department_name VARCHAR(30) NOT NULL   
 );
 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL, 
-    department_id INT,
+    department_id INT NOT NULL,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE CASCADE
@@ -22,16 +22,9 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT,
+    role_id INT NOT NULL,
+    manager_id BOOLEAN, 
     FOREIGN KEY (role_id)
     REFERENCES role(id)
     ON DELETE CASCADE
-);
-
-CREATE TABLE manager (   
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
-    ON DELETE CASCADE 
 );
