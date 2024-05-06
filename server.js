0d900f6b3d42d0ed34b3729c3dba3cbd855f552b
Felix Teng <fsmunozteng@gmail.com>
2024-05-06 00:35:09 -0700

    reversed updates with env and sequelize. also created server.js file to establish connection to db

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL Username
+    user: 'root',
+    // TODO: Add MySQL Password
+    password: 'Acceptance2!',
+    database: 'books_db'
+  },
+  console.log(`Connected to the employee_db database.`)
+);
+
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
